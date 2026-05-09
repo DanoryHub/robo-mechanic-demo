9e78c89a608d89ffbae125149624b287ee993c0f
Ivan Miatselski <danorywork@gmail.com>
2026-05-09 22:45:14 +0300

    Add initial character logic (will drastically rework in future iterations)

---
+++ scripts/battle_field/BS_battle_field.gd
@@ -0,0 +1,19 @@
+extends Node2D
+@onready var player: CharacterBody2D = $Player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_ready() -> void:
+	if not player:
+		player = $Player
+	
+	player.outer_call_test()
---
+++ scripts/battle_field/BS_player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+	
+func outer_call_test():
+	print("Its working !")
+
+func _physics_process(delta: float) -> void:
+	
+	var direction: Vector2 = Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
+	
+	if direction.x:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction.y:
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
